1165e1dbe8f73879114407140e55e629dbd869eb
AbdulRauf Siddiqui <abdulrauf_99@outlook.com>
2025-07-06 20:31:16 +0500

    modified startup.sh

--- terra/startup.sh
+++ terra/startup.sh
@@ -4,6 +4,7 @@
     mkdir -p /etc/gcp
     sudo apt-get update && sudo apt-get install -y git
     git clone https://github.com/AbdulRauf-Sidd/PUB-SUB-Terraform.git /opt/flask-app
+    touch gitcloned.txt
 
     # Write service account key to file from metadata
     export WEATHER_API_KEY=$(curl -s -H "Metadata-Flavor: Google" \
@@ -14,10 +15,12 @@
     "http://metadata.google.internal/computeMetadata/v1/instance/attributes/PUBSUB_TOPIC_ID")
     export GOOGLE_CREDENTIALS=$(curl -s -H "Metadata-Flavor: Google" \
     "http://metadata.google.internal/computeMetadata/v1/instance/attributes/GOOGLE_CREDENTIALS")
+    touch metadata.txt
 
 
     echo "$GOOGLE_CREDENTIALS" > /etc/gcp/key.json
     export GOOGLE_APPLICATION_CREDENTIALS="/etc/gcp/key.json"
+    touch googleapplicationcredentials.txt
 
     # Write .env file
     cat <<EOF > /opt/flask-app/.env
@@ -25,10 +28,11 @@
     GCP_PROJECT_ID="$${GCP_PROJECT_ID}"
     PUBSUB_TOPIC_ID="$${PUBSUB_TOPIC_ID}"
     EOF
-
+    touch envfile.txt
+    
     # Clone and start app
     cd /opt/flask-app
-    sudo apt-get update
+    # sudo apt-get update
     touch aptupdated.txt
     sudo apt-get install -y python3-pip
     touch pipinstalled.txt
